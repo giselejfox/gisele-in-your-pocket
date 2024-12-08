a79d667c5b04caad3ec76e48453f1f17cf7ac310
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2024-12-08 14:08:11 -0800

    basics are down!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
+import React, { useRef } from 'react';
 
 function App() {
+  const audioRef = useRef(null);  // Create a reference to the audio element
+
+  const playSound = () => {
+    audioRef.current.play();  // Play the sound when the image is clicked
+  };
+
   return (
-    <div className="App">
-      test
+    <div className="container" onClick={playSound}>
+      <img src="img/gisele-face.png" alt="gisele's face"/>
+      Click me!
+      <audio ref={audioRef} src="sounds/sound1.mp3" />
     </div>
   );
 }
